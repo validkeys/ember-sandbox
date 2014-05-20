3c11e09a2a88ad53a8360865937663cfefc15312
Kyle <validkeys@gmail.com>
2014-05-20 16:28:29 -0400

    no message

---
+++ atjs/Brocfile.js
@@ -0,0 +1,39 @@
+/* global require, module */
+var mergeTrees = require('broccoli-merge-trees');
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+var app = new EmberApp({
+  name: require('./package.json').name,
+
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  },
+
+  getEnvJSON: require('./config/environment')
+});
+
+// Use this to add additional libraries to the generated output files.
+app.import('vendor/ember-data/ember-data.js');
+
+app.import('vendor/Caret.js/src/jquery.caret.js');
+app.import('vendor/jquery.atwho/dist/js/jquery.atwho.js');
+app.import('vendor/jquery.atwho/dist/css/jquery.atwho.css');
+
+// If the library that you are including contains AMD or ES6 modules that
+// you would like to import into your application please specify an
+// object with the list of modules as keys along with the exports of each
+// module as its value.
+app.import('vendor/ic-ajax/dist/named-amd/main.js', {
+  'ic-ajax': [
+    'default',
+    'defineFixture',
+    'lookupFixture',
+    'raw',
+    'request',
+  ]
+});
+
+
+module.exports = app.toTree();
